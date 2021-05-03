84881985b5432771b109594f7cd8a30d5e6a2ee0
skiran017 <skiran017@gmail.com>
2021-05-03 18:40:07 +0530

    final
    -HTML canvas
    -function to move the block with keys
    -link(image) , moves according to direction , useState, useEffect and useRef
    -custom hook (useMovement())

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import "./App.css";
+import useMovement from "./useMovement";
 
 export default function App() {
+  const canvasRef = useRef(null);
+  const linkDownRef = useRef(null);
+  const linkLeftRef = useRef(null);
+  const linkUpRef = useRef(null);
+  const linkRightRef = useRef(null);
+  const { x, y, direction, move } = useMovement();
+
+  //set height and width of canvas
+  useEffect(() => {
+    const context = canvasRef.current.getContext("2d");
+
+    context.canvas.height = window.innerHeight;
+    context.canvas.width = window.innerWidth;
+  }, []);
+
+  //move the box if x || y changes
+  useEffect(() => {
+    const context = canvasRef.current.getContext("2d");
+    context.clearRect(0, 0, window.innerHeight, window.innerWidth);
+    // context.fillRect(x, y, 100, 100);
+    let theLinkRef;
+    if (direction === "up") theLinkRef = linkUpRef;
+    if (direction === "left") theLinkRef = linkLeftRef;
+    if (direction === "down") theLinkRef = linkDownRef;
+    if (direction === "right") theLinkRef = linkRightRef;
+
+    context.drawImage(theLinkRef.current, x, y);
+  }, [x, y]);
+
   return (
     <div className="app">
-      <canvas />
+      <canvas ref={canvasRef} />
 
       <div className="arrows">
-        <button>Up</button>
-        <button>Left</button>
-        <button>Down</button>
-        <button>Right</button>
+        <button onClick={() => move("up")}>Up</button>
+        <button onClick={() => move("left")}>Left</button>
+        <button onClick={() => move("down")}>Down</button>
+        <button onClick={() => move("right")}>Right</button>
       </div>
 
       <div className="images">
-        <img src="https://i.imgur.com/JYUB0m3.png" alt="Down" />
-        <img src="https://i.imgur.com/GEXD7bk.gif" alt="Right" />
-        <img src="https://i.imgur.com/XSA2Oom.gif" alt="Up" />
-        <img src="https://i.imgur.com/4LGAZ8t.gif" alt="Left" />
+        <img
+          ref={linkDownRef}
+          src="https://i.imgur.com/JYUB0m3.png"
+          alt="Down"
+        />
+        <img
+          ref={linkRightRef}
+          src="https://i.imgur.com/GEXD7bk.gif"
+          alt="Right"
+        />
+        <img ref={linkUpRef} src="https://i.imgur.com/XSA2Oom.gif" alt="Up" />
+        <img
+          ref={linkLeftRef}
+          src="https://i.imgur.com/4LGAZ8t.gif"
+          alt="Left"
+        />
       </div>
     </div>
   );
